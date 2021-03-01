20362203afcfa19d3205777c7d3c079254b2c4db
Sid-Bhatia-0 <32610387+Sid-Bhatia-0@users.noreply.github.com>
2021-03-01 16:04:43 +0530

    create basic structure for this package (#1)
    * set julia compat to "1"
    
    * add and load packages
    
    * import instead of using in runtests.jl
    
    * optionally load render.jl if Makie is present
    
    * create examples/colliding_balls.jl

---
+++ examples/colliding_balls.jl
@@ -0,0 +1,11 @@
+import DifferentiablePhysics2D
+import DifferentiablePhysics2D: DP2D
+import PhysicsPrimitives2D
+import PhysicsPrimitives2D: PP2D
+import StaticArrays
+const SA = StaticArrays
+import LinearAlgebra
+const LA = LinearAlgebra
+import ForwardDiff
+const FD = ForwardDiff
+import Makie
--- src/DifferentiablePhysics2D.jl
+++ src/DifferentiablePhysics2D.jl
@@ -1,5 +1,20 @@
 module DifferentiablePhysics2D
 
-# Write your package code here.
+const DP2D = DifferentiablePhysics2D
+export DP2D
+
+import PhysicsPrimitives2D
+import PhysicsPrimitives2D: PP2D
+import StaticArrays
+const SA = StaticArrays
+import LinearAlgebra
+const LA = LinearAlgebra
+import ForwardDiff
+const FD = ForwardDiff
+import Requires
+
+function __init__()
+    Requires.@require Makie = "ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("render.jl")
+end
 
 end
