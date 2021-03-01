8f411eb8224de0b90d5a77c4b5c53c6dc7b3e6d1
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-03-01 15:32:58 +0530

    add and load packages

--- src/DifferentiablePhysics2D.jl
+++ src/DifferentiablePhysics2D.jl
@@ -1,5 +1,16 @@
 module DifferentiablePhysics2D
 
-# Write your package code here.
+const DP2D = DifferentiablePhysics2D
+export DP2D
+
+import PhysicsPrimitives2D
+import PhysicsPrimitives2D: PP2D
+import StaticArrays
+const SA = StaticArrays
+import LinearAlgebra
+const LA = LinearAlgebra
+import ForwardDiff
+const FD = ForwardDiff
+import Requires
 
 end
