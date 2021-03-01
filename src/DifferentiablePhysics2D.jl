722820c09cf2f9eb80d4749f3f42edfe26b9eef3
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-03-01 15:40:00 +0530

    optionally load render.jl if Makie is present

--- src/DifferentiablePhysics2D.jl
+++ src/DifferentiablePhysics2D.jl
@@ -13,4 +13,8 @@ import ForwardDiff
 const FD = ForwardDiff
 import Requires
 
+function __init__()
+    Requires.@require Makie = "ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("render.jl")
+end
+
 end
