6a2b1fa01f947bc859cf041ff50179d8ae416dc0
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-03-12 15:02:42 +0530

    WIP colliding balls

--- examples/colliding_balls.jl
+++ examples/colliding_balls.jl
@@ -1,64 +1,47 @@
-import DifferentiablePhysics2D
-import DifferentiablePhysics2D: DP2D
-import PhysicsPrimitives2D
-import PhysicsPrimitives2D: PP2D
 import StaticArrays
 const SA = StaticArrays
 import LinearAlgebra
 const LA = LinearAlgebra
 import ForwardDiff
 const FD = ForwardDiff
-# import Makie
 
-const T = Float32
-const STEP_SIZE = one(T)
+T = Float32
+step_size = convert(T, 1)
 
-const RADIUS_BALL1 = one(T)
-const SHAPE_BALL1 = PP2D.StdCircle(RADIUS_BALL1)
-const POSITION_BALL1 = zero(SA.SVector{2, T})
-const POSITION_CHANGE_BALL1 = zero(SA.SVector{2, T})
-const VELOCITY_BALL1 = SA.SVector(one(T), zero(T))
-const VELOCITY_CHANGE_BALL1 = zero(SA.SVector{2, T})
-const MASS_BALL1 = one(T)
-const INV_MASS_BALL1 = one(T)
+r1 = convert(T, 1)
+m1 = convert(T, 1)
+p1 = zero(SA.SVector{2, T})
+v1_mag = FD.Dual(convert(T, 2), (one(T), zero(T)))
+v1_theta = FD.Dual(convert(T, pi / 6), (zero(T), one(T)))
+v1 = SA.SVector(v1_mag * cos(v1_theta), v1_mag * sin(v1_theta))
 
-const RADIUS_BALL2 = one(T)
-const SHAPE_BALL2 = PP2D.StdCircle(RADIUS_BALL2)
-const POSITION_BALL2 = SA.SVector(convert(T, 3.5), zero(T))
-const POSITION_CHANGE_BALL2 = zero(SA.SVector{2, T})
-const VELOCITY_BALL2 = SA.SVector(zero(T), zero(T))
-const VELOCITY_CHANGE_BALL2 = zero(SA.SVector{2, T})
-const MASS_BALL2 = one(T)
-const INV_MASS_BALL2 = one(T)
+d = convert(T, 4)
+r2 = convert(T, 2)
+m2 = convert(T, 2)
+p2 = SA.SVector(d, zero(T))
+v2 = zero(SA.SVector{2, T})
 
-function move(p1, v1, p2, v2)
-    p1_new = p1 .+ (STEP_SIZE * v1)
-    p2_new = p2 .+ (STEP_SIZE * v2)
-    return p1_new, v1, p2_new, v2
+function move(position, velocity, step_size)
+    position_new = position .+ (step_size * velocity)
+    return position_new, velocity
 end
 
-p1 = POSITION_BALL1
-v1 = VELOCITY_BALL1
-p2 = POSITION_BALL2
-v2 = VELOCITY_BALL2
 t = 0
-
 @show t
 @show p1
 @show v1
 @show p2
 @show v2
-@show PP2D.is_colliding(SHAPE_BALL1, SHAPE_BALL2, p2 .- p1)
 println("********************************************************************")
 
-for i in 1:2
-    global p1, v1, p2, v2 = move(p1, v1, p2, v2)
+for i in 1:4
+    global p1, v1 = move(p1, v1, step_size)
+    global p2, v2 = move(p2, v2, step_size)
     global t += 1
     @show t
     @show p1
     @show v1
     @show p2
     @show v2
-    @show PP2D.is_colliding(SHAPE_BALL1, SHAPE_BALL2, p2 .- p1)
     println("********************************************************************")
 end
